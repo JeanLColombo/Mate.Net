03f4398ebc1872fe3c850b6a26e5227499e1122f
Jean Luca Colombo <jeanlucacolombo@outlook.com>
2020-05-13 18:07:12 -0300

    Uptade Game.cs

--- Source/Game.cs
+++ Source/Game.cs
@@ -4,6 +4,8 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using Mate.Pieces;
+using System.Text.RegularExpressions;
+using System.Linq;
 
 namespace Mate
 {
@@ -19,6 +21,8 @@ namespace Mate
 
         public IReadOnlyCollection<Piece> BlackPieces { get => chess.Black.Pieces; }
 
+        public Outcome Outcome { get; private set; } = Outcome.Game;
+
         /// <summary>
         /// Initializes a <see cref="Game"/> of <see cref="Chess"/> with a Standard <see cref="Piece"/> setup.
         /// </summary>
@@ -33,13 +37,31 @@ namespace Mate
         /// </summary>
         /// <param name="customPieces">Customizable <see cref="PieceInput"/>.</param>
         public Game(CustomPieceInput customPieces) =>
-            customInitialization(customPieces);
+            CustomInitialization(customPieces);
         
+        /// <summary>
+        /// Process the <paramref name="moveIndex"/>-th <see cref="Move"/> available from a player, based on <see cref="PlayerTurn"/>. Also calls <see cref="UpdateOutcome()"/>.
+        /// </summary>
+        /// <param name="moveIndex"></param>
+        /// <returns></returns>
+        public Outcome ProcessMove(uint moveIndex)
+        {
+            if (Outcome != Outcome.Game)
+                return Outcome;
 
-        //TODO: Create a move processing extension.
+            var availableMoves = chess.LegalMoves(PlayerTurn);
 
+            if (moveIndex >= availableMoves.Count)
+            {
+                throw new ArgumentOutOfRangeException("Move Index is greater than Move.Count", nameof(moveIndex));
+            }
 
-        private void customInitialization(CustomPieceInput customPieces)
+            ProcessMove(availableMoves.ElementAt((int)moveIndex));
+
+            return UpdateOutcome();
+        }
+
+        private void CustomInitialization(CustomPieceInput customPieces)
         {
             foreach (PieceInput piece in customPieces)
             {
@@ -69,6 +91,74 @@ namespace Mate
                     default:
                         break;
                 }
+            }
+        }
+
+        /// <summary>
+        /// Toggles <see cref="Game.PlayerTurn"/>. If it is <see cref="WhitePieces"/> turn, also increases <see cref="Game.CurrentMove"/> by one.
+        /// </summary>
+        private void UpdatePlayerTurn()
+        {
+            PlayerTurn = PlayerTurn ? false : true;
+            if (PlayerTurn)
+                CurrentMove++;
+        }
+
+        /// <summary>
+        /// Checks for <see cref="Outcome.Stalemate"/>, <see cref="Outcome.MateWhite"/> or <see cref="Outcome.MateBlack"/>, while also calling <see cref="UpdatePlayerTurn"/>.
+        /// </summary>
+        /// <returns></returns>
+        private Outcome UpdateOutcome()
+        {
+            UpdatePlayerTurn();
+
+            if (chess.LegalMoves(PlayerTurn).Count == 0)
+            {
+                if (PlayerTurn ? chess.White.IsChecked() : chess.Black.IsChecked())
+                    Outcome = PlayerTurn ? Outcome.MateWhite : Outcome.MateBlack;
+                else
+                    Outcome = Outcome.Stalemate;
+            }
+
+            return Outcome;
+        }
+
+        private void ProcessMove(Move move)
+        {
+
+            Piece captured = null;
+
+            switch (move.Item3) 
+            {
+                case SpecialMoves.None:
+                    captured = move.Item1.MoveTo(move.Item2);
+                    break;
+                case SpecialMoves.KingSideCastle:
+                    //TODO: Implement SpecialMoves
+                    break;
+                case SpecialMoves.QueenSideCaste:
+                    break;
+                case SpecialMoves.Passant:
+                    break;
+                case SpecialMoves.PromoteToBishop:
+                    break;
+                case SpecialMoves.PromoteToKnight:
+                    break;
+                case SpecialMoves.PromoteToRook:
+                    break;
+                case SpecialMoves.PromoteToQueen:
+                    break;
+                default:
+                    break;
+            }
+
+            if (captured != null)
+            {
+                var playerOne = PlayerTurn ? chess.White : chess.Black;
+                var plaerTwo = PlayerTurn ? chess.Black : chess.White;
+
+                playerOne.Captured.Add(captured);
+                plaerTwo.Pieces.Remove(captured);
             }
         }
     }
